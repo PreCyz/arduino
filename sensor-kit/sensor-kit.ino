bc49bc4fce0901094badd662afa1d28ddb6ba790
Precyzik <gawaprecyz@gmail.com>
2025-08-31 16:17:33 +0200

    Reoragnizing display

--- sensor-kit/sensor-kit.ino
+++ sensor-kit/sensor-kit.ino
@@ -16,6 +16,7 @@ int pot_value;
 bool button_state;
 int mic_value;
 int light_value;
+float preassure;
 
 void setup() {
   Serial.begin(9600);
@@ -32,38 +33,28 @@ void setup() {
 
   Oled.begin();
   Oled.setFlipMode(true);
+  Oled.setFont(u8x8_font_amstrad_cpc_extended_r); 
 
   Accelerometer.begin();
   Pressure.begin();
 }
 
 void loop() {
-  Oled.setFont(u8x8_font_amstrad_cpc_extended_r); 
-
   //cursor values are in characters, not pixels
-  Oled.setCursor(0, 4);
-  
+  Oled.setCursor(0, 5);
   // If accelerometer and altimeter are queried too close to one another
   // this causes a hang, so we read this first.
   Oled.print("x:"); 
   Oled.print(Accelerometer.readX()); 
   Oled.print(" y:"); 
   Oled.print(Accelerometer.readY());        
-  Oled.setCursor(0, 5);
+  Oled.setCursor(0, 6);
   Oled.print("z:"); 
   Oled.print(Accelerometer.readZ());
-  Oled.print(" T:");
-  Oled.print(Environment.readTemperature());
-  Oled.print("C");
-
-  Oled.setCursor(0, 0);
-  Oled.print("But:"); 
 
   pot_value = analogRead(POT);
-  
   button_state = digitalRead(BUTTON);
-  Oled.print(button_state); 
-  
+
   if (button_state == true) {
     digitalWrite(LED, HIGH);
     tone(BUZZER, pot_value);
@@ -71,32 +62,43 @@ void loop() {
     digitalWrite(LED, LOW);
     noTone(BUZZER);
   }
-  
-  Oled.setCursor(0, 1);
-  Oled.print("BuzPot: ");
-  Oled.print(pot_value);
-  Oled.print("Hz  ");
 
-  mic_value = analogRead(MIC);
-  Oled.setCursor(0, 2);
-  Oled.print("Mic: ");
-  Oled.print(mic_value);
-  Oled.print("   ");
-
-  light_value = analogRead(LIGHT);
   Oled.setCursor(0, 3);
-  Oled.print("Light: ");
-  Oled.print(light_value);
-  Oled.print("   ");
+  Oled.print("Bt:"); 
+  Oled.print(button_state);
+  Oled.print(" BzPt: ");
+  Oled.print(pot_value);
+  Oled.print("Hz");
 
-  Oled.setCursor(0, 6);
+  Oled.setCursor(0, 0);
+  Oled.print("Tmp: "); 
+  Oled.print(Pressure.readTemperature());
+  Oled.print(" ");
+  Oled.print(Environment.readTemperature());
+
+  preassure = Pressure.readPressure()/100;
+  Oled.setCursor(0, 1);
+  Oled.print("P: "); 
+  Oled.print(preassure);
+  Oled.print("hPA"); 
+
+  Oled.setCursor(0, 2);
   Oled.print("Hum: ");
   Oled.print(Environment.readHumidity());
   Oled.print("%");
 
+  light_value = analogRead(LIGHT);
+  mic_value = analogRead(MIC);
+  Oled.setCursor(0, 4);
+  Oled.print("L:");
+  Oled.print(light_value);
+  Oled.print(" M:");
+  Oled.print(mic_value);
+
   Oled.setCursor(0, 7);
   Oled.print("Alt:"); 
   Oled.print(Pressure.readAltitude());
+  Oled.print("m"); 
 
   delay(100);
 }
